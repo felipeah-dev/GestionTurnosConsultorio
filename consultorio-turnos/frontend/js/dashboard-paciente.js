ff180c071a1f7931d60815d9a14d28e83c73e95d
felipeah-dev <andradehernandezfelipe@gmail.com>
2026-03-02 10:33:37 -0700

    poner ruta verdadera de index.html si entras al dashboard sin token

--- consultorio-turnos/frontend/js/dashboard-paciente.js
+++ consultorio-turnos/frontend/js/dashboard-paciente.js
@@ -6,7 +6,7 @@
 const token = localStorage.getItem("token");
 
 if (!token) {
-    window.location.href = "index.html";
+    window.location.href = "../../index.html";
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
